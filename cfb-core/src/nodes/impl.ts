7c2550efd1bd84bc6819e7c00adadc35f2e23b4e
carefree0910 <syameimaru.saki@gmail.com>
2024-11-02 19:55:37 +0800

    ✨Implemented `makeSingleNode`

--- cfb-core/src/nodes/impl.ts
+++ cfb-core/src/nodes/impl.ts
@@ -1,4 +1,8 @@
-import { registerGroupNode, registerSingleNode } from "./impl/base.ts";
+import type { ISingleNodeR } from "./types.ts";
+import type { Matrix2D } from "../toolkit.ts";
+
+import { v4 } from "uuid";
+import { NODE_FACTORY, registerGroupNode, registerSingleNode } from "./impl/base.ts";
 import { Group } from "./impl/group.ts";
 import { ImageNode } from "./impl/image.ts";
 import { RectangleNode } from "./impl/shape/rectangle.ts";
@@ -12,3 +16,29 @@ export * from "./impl/group.ts";
 registerSingleNode("rectangle", RectangleNode);
 registerSingleNode("image", ImageNode);
 registerGroupNode("group", Group);
+
+export interface IMakeSingleNode<T extends ISingleNodeR> {
+  type: T["type"];
+  uuid?: string;
+  alias: string;
+  transform: Matrix2D;
+  params: T["params"];
+  z: number;
+}
+export function makeSingleNode<T extends ISingleNodeR>({
+  type,
+  uuid,
+  alias,
+  transform,
+  params,
+  z,
+}: IMakeSingleNode<T>): T {
+  return NODE_FACTORY.fromJsonData({
+    type,
+    alias,
+    uuid: uuid ?? v4(),
+    transform,
+    params,
+    z,
+  });
+}
