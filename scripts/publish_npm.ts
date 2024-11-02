4f20fa31d501eafa3c545d3882a74513866642c3
carefree0910 <syameimaru.saki@gmail.com>
2024-11-02 17:39:08 +0800

    🔨🚧Introduced `check` & `publish` CI

---
+++ scripts/publish_npm.ts
@@ -0,0 +1,14 @@
+import { which } from "https://deno.land/x/which@0.3.0/mod.ts";
+import { exec } from "./utils.ts";
+
+const npmExecPath = await which("npm");
+if (!npmExecPath) {
+  console.error("npm not found");
+  Deno.exit(1);
+}
+const packages = ["cfb-core", "cfb-svg", "cfb-web"];
+await Promise.all(
+  packages.map((pkg) =>
+    exec(new Deno.Command(npmExecPath, { args: ["publish", `./${pkg}/npm/`] }))
+  ),
+);
