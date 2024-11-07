8356c7d8418abbd9904aad4ce4484a3b4490c4fa
carefree0910 <syameimaru.saki@gmail.com>
2024-11-07 15:40:36 +0800

    ✅Enhanced `ui`

--- cfb-web/src/ui.ts
+++ cfb-web/src/ui.ts
@@ -1,11 +1,15 @@
 import type {
-  IMakeNode,
-  ISingleNodeR,
+  IGroup,
+  IGroupNode,
+  IMakeGroupNode,
   ISolidFillParams,
+  ITextNode,
   IWorld,
+  Matrix2DFields,
 } from "@carefree0910/cfb-core";
 
 import {
+  BBox,
   DirtyStatus,
   ExecuterPlugin,
   Logger,
@@ -14,16 +18,21 @@ import {
   registerExecuterEvent,
   TargetQueue,
 } from "@carefree0910/cfb-core";
+import { getFontWH } from "./utils.ts";
 
 function setFill(
-  node: ISingleNodeR,
+  node: IGroup,
   world: IWorld,
   color: string,
   opacity: number,
 ): Promise<void> {
-  const fillParams = node.params.fillParamsList![0] as ISolidFillParams;
+  const children = node.children;
+  const bg = children[0];
+  const text = children[1] as ITextNode;
+  const fillParams = bg.params.fillParamsList![0] as ISolidFillParams;
   fillParams.color = color;
   fillParams.opacity = opacity;
+  text.params.opacity = Math.min(opacity * 2, 1);
   world.setRenderInfo(node.alias, {
     dirtyStatus: DirtyStatus.CONTENT_DIRTY,
     targetQueue: TargetQueue.IMMEDIATE,
@@ -31,6 +40,16 @@ function setFill(
   return Promise.resolve();
 }
 
+function getCenterTransform(
+  content: string,
+  fontSize: number,
+  bounding: Matrix2D,
+): Matrix2DFields {
+  const { w, h } = getFontWH({ content, fontSize });
+  let bbox = BBox.from(w, h);
+  bbox = bbox.move(new BBox(bounding).center.subtract(bbox.center));
+  return bbox.fields;
+}
 function makeButton(
   alias: string,
   transform: Matrix2D,
@@ -41,15 +60,37 @@ function makeButton(
   makeUIElement({
     store: { activated: false },
     nodeData: {
-      type: "rectangle",
-      alias,
-      transform,
-      params: {
-        tag: "ui",
-        fillParamsList: [{ type: "color", color: deactivatedColor, opacity: 0.25 }],
-      },
+      type: "group",
+      alias: `${alias}Button`,
+      params: { tag: "ui" },
+      transform: Matrix2D.identity(),
       z: 0,
-    } as IMakeNode<ISingleNodeR>,
+      children: [
+        {
+          type: "rectangle",
+          alias: `${alias}ButtonBackground`,
+          transform,
+          params: {
+            tag: "ui",
+            fillParamsList: [{ type: "color", color: deactivatedColor, opacity: 0.1 }],
+          },
+          z: 0,
+        },
+        {
+          type: "text",
+          alias: `${alias}ButtonText`,
+          transform: getCenterTransform(alias, 24, transform),
+          params: {
+            tag: "ui",
+            content: alias,
+            fontSize: 22,
+            fontWeight: "bold",
+            opacity: 0.2,
+          },
+          z: -1,
+        },
+      ],
+    } as IMakeGroupNode<IGroupNode>,
     callbacks: {
       onBind: ({ world, store }) => {
         registerExecuterEvent(() => {
@@ -64,21 +105,21 @@ function makeButton(
         if (store.getter("activated")) {
           setFill(node, world, activatedColor, 0.25);
         } else {
-          setFill(node, world, deactivatedColor, 0.25);
+          setFill(node, world, deactivatedColor, 0.1);
         }
       },
       onEnter: ({ node, world, store }) => {
         if (store.getter("activated")) {
           setFill(node, world, activatedColor, 0.4);
         } else {
-          setFill(node, world, deactivatedColor, 0.25);
+          setFill(node, world, deactivatedColor, 0.1);
         }
       },
       onPress: ({ node, world, store }) => {
         if (store.getter("activated")) {
           setFill(node, world, activatedColor, 0.6);
         } else {
-          setFill(node, world, deactivatedColor, 0.25);
+          setFill(node, world, deactivatedColor, 0.1);
         }
       },
       onClick: ({ world }) => {
@@ -98,7 +139,7 @@ const redoColor = "#00ffff";
 const deactivatedColor = "#000000";
 
 makeButton(
-  "undo",
+  "Undo",
   Matrix2D.from(200, 50, 80, 50),
   undoColor,
   (executer) => executer?.canUndo() ?? false,
@@ -109,7 +150,7 @@ makeButton(
   },
 );
 makeButton(
-  "redo",
+  "Redo",
   Matrix2D.from(300, 50, 80, 50),
   redoColor,
   (executer) => executer?.canRedo() ?? false,
